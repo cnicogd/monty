70d193c3a62a097c0ccc6be4084000cfa84ac523
Nicolas Gonzalez Delgado <cnicogd@gmail.com>
2021-05-13 16:13:54 -0500

    header

---
+++ monty.h
@@ -0,0 +1,13 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#endif
